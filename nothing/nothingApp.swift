5110527e9ef7cc46b5653ea5afe95ccd7b816155
carrie <771685809@qq.com>
2024-12-19 10:01:40 +0800

    Initial Commit

---
+++ nothing/nothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  nothingApp.swift
+//  nothing
+//
+//  Created by  玉城 on 2024/12/19.
+//
+
+import SwiftUI
+
+@main
+struct nothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
