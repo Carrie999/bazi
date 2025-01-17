0a985527510bb099817af00f9f2560221780aa30
carrie <771685809@qq.com>
2025-01-17 13:54:53 +0800

    first commit

---
+++ nothing/baziSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  baziSwiftUI.swift
+//  nothing
+//
+//  Created by  玉城 on 2024/12/19.
+//
+
+import SwiftUI
+
+struct baziSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    baziSwiftUI()
+}
